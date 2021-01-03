7dc4f7f1a1a621f6af134d3fbd87eb4fa33a0bc0
Vyacheslav Efimov <efimov@element-soft.com>
2021-01-03 14:45:23 +0300

    Project complete

--- Flag Viewer/ViewController.swift
+++ Flag Viewer/ViewController.swift
@@ -7,11 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.0647065416, green: 0.3385555148, blue: 1, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+        
+        title = "Countries"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png") {
+                pictures.append(item)
+                countries.append(String(item.dropLast(4)))
+            }
+        }
+        
+        print(countries)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flagCell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let dvc = storyboard?.instantiateViewController(identifier: "detailController") as? DetailViewController {
+            dvc.picture = pictures[indexPath.row]
+            dvc.country = countries[indexPath.row]
+            navigationController?.pushViewController(dvc, animated: true)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
